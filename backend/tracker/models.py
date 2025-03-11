6c91eebab2531e2d3dc8aa1b03f66d0793713e0d
diego orellana <diego.orellana.git@gmail.com>
2025-03-11 16:03:25 -0300

    created tracker app

---
+++ backend/tracker/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class MacroPlan(models.Model):
+  name = models.CharField(max_length=255)
+  target_calories = models.DecimalField(max_digits=6, decimal_places=2)
+  target_carbs = models.DecimalField(max_digits=6, decimal_places=2)
+  target_proteins = models.DecimalField(max_digits=6, decimal_places=2)
+  target_fats = models.DecimalField(max_digits=6, decimal_places=2)
