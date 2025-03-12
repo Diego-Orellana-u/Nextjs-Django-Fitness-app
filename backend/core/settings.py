41edf16529098a5b0ea3d0712d765d8dfc337047
diego orellana <diego.orellana.git@gmail.com>
2025-03-12 15:25:32 -0300

    added new fields in macroplans and users

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "tracker.CustomUser"
 
 # Application definition
 
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tracker',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'new_password'
     }
 }
 
--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -1,9 +1,31 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
+from .managers import CustomUserManager
 
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
 class MacroPlan(models.Model):
   name = models.CharField(max_length=255)
   target_calories = models.DecimalField(max_digits=6, decimal_places=2)
   target_carbs = models.DecimalField(max_digits=6, decimal_places=2)
   target_proteins = models.DecimalField(max_digits=6, decimal_places=2)
   target_fats = models.DecimalField(max_digits=6, decimal_places=2)
+
+  user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
