b48d348be1b04abbf54f7f7813e3822d45afe1fb
diego orellana <diego.orellana.git@gmail.com>
2025-05-27 07:20:05 -0400

    added urls

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import FoodItem
 
 # Create your views here.
 @api_view(['GET'])
 def get_food(request):
+  # if request.method == 'GET':
+    # queryset = FoodItem.objects.
   return Response('ok')
