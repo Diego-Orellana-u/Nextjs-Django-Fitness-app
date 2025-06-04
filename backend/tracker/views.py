174184602a291fdbfccf535da262450cef605827
diego orellana <diego.orellana.git@gmail.com>
2025-06-04 09:33:25 -0400

    added post method to product_detail method

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,20 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import FoodItem
-from .serializers import productSerializer
+from .serializers import ProductSerializer
 
 # Create your views here.
 @api_view(['GET'])
 def product_brand_list(request, brand):
   if request.method == 'GET':
     products = FoodItem.objects.filter(brand=brand)
-    serializer = productSerializer(products, many=True)
+    serializer = ProductSerializer(products, many=True)
   return Response(serializer.data)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_detail(request, id):
   if request.method == 'GET':
-    product = FoodItem.objects.get(pk=id)
-    serializer = productSerializer(product)
+    product = get_object_or_404(FoodItem, pk=id)
+    serializer = ProductSerializer(product)
 
+  elif request.method == 'POST':
+    serializer = ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
   return Response(serializer.data)
