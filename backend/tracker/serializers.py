010871254c6b31588feb6c2f006a86c9cb2d7ac4
diego orellana <diego.orellana.git@gmail.com>
2025-06-05 08:04:32 -0400

    added readonly to auto_now fields in serializer

--- backend/tracker/serializers.py
+++ backend/tracker/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import FoodItem
-
 class ProductSerializer(serializers.Serializer):
   id = serializers.IntegerField()
   name = serializers.CharField(max_length=200)
@@ -11,7 +10,9 @@ class ProductSerializer(serializers.Serializer):
   carbs_per_100g = serializers.DecimalField(max_digits=7, decimal_places=2,)
   fat_per_100g = serializers.DecimalField(max_digits=7, decimal_places=2,)
   common_serving_size_grams = serializers.DecimalField(max_digits=7, decimal_places=2, help_text="Weight in grams for the common serving size (if applicable)")
+  created_at = serializers.DateTimeField(read_only=True)
+  updated_at = serializers.DateTimeField(read_only=True)
 
+  
   def create(self, validated_data):
     return FoodItem.objects.create(**validated_data)
-  
