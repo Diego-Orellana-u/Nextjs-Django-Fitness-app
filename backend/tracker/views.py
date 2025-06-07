addbff1768203ace4ed443025e6734d1a035ca42
diego orellana <diego.orellana.git@gmail.com>
2025-06-07 08:21:44 -0400

    added delete method to product_detail

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import FoodItem
 from .serializers import ProductSerializer
@@ -12,14 +13,18 @@ def product_brand_list(request, brand):
     serializer = ProductSerializer(products, many=True)
   return Response(serializer.data)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'DELETE'])
 def product_detail(request, id):
+  product = get_object_or_404(FoodItem, pk=id)
   if request.method == 'GET':
-    product = get_object_or_404(FoodItem, pk=id)
     serializer = ProductSerializer(product)
 
   elif request.method == 'POST':
     serializer = ProductSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
+
+  elif request.method == 'DELETE':
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
   return Response(serializer.data)
