a1144a39373d392f6a948377a951eb28d26234c3
diego orellana <diego.orellana.git@gmail.com>
2025-06-09 13:23:46 -0400

    created nutritiongoals model serializer

--- backend/tracker/serializers.py
+++ backend/tracker/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from .models import FoodItem
+from .models import FoodItem, NutritionGoal
 class ProductSerializer(serializers.ModelSerializer):
   class Meta:
     model = FoodItem
     fields = ['name','brand','off_barcode','calories_per_100g', 'protein_per_100g', 'carbs_per_100g', 'fat_per_100g', 'common_serving_size_grams', 'created_at', 'updated_at']
+
+
+class NutriGoalsSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = NutritionGoal
+    fields = ['name', 'target_calories', 'target_carbs', 'target_proteins', 'target_fats', 'is_active', 'start_date', 'user']
